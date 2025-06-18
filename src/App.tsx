ac653596cd491037a4beccd60806664125e1f196
Bruno Pereira <brunopereira@me.com>
2025-06-19 00:21:04 +0100

    final commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,34 @@
 import React, { FC } from "react";
+import {} from "react-redux";
 import "./App.css";
+import { useAppDispatch, useAppSelector } from "./store/hooks";
+import { RootState } from "./store";
+import Search from "./components/Search";
+import Alert from "./components/Alert";
+import Weather from "./components/Weather";
+import { setAlert } from "./store/actions/alertActions";
+import { setError } from "./store/actions/weatherActions";
 
 const App: FC = () => {
-  return <div className="App"></div>;
+  const dispatch = useAppDispatch();
+  const weatherData = useAppSelector((state: RootState) => state.weather.data);
+  const loading = useAppSelector((state: RootState) => state.weather.loading);
+  const error = useAppSelector((state: RootState) => state.weather.error);
+  const alertMsg = useAppSelector((state: RootState) => state.alert.message);
+  return (
+    <div className="has-text-centered">
+      <Search title="Enter city name and press search" />
+      {loading ? (
+        <h2 className="is-size-3 py2">Loading...</h2>
+      ) : (
+        weatherData && <Weather data={weatherData} />
+      )}
+      {alertMsg && (
+        <Alert message={alertMsg} onClose={() => dispatch(setAlert(""))} />
+      )}
+      {error && <Alert message={error} onClose={() => dispatch(setError())} />}
+    </div>
+  );
 };
 
 export default App;
--- src/index.tsx
+++ src/index.tsx
@@ -1,12 +1,18 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 import App from "./App";
+// import { Provider } from "react";
+import store from "./store";
+import "../node_modules/bulma/css/bulma.min.css";
+import { Provider } from "react-redux";
 
 const root = ReactDOM.createRoot(
   document.getElementById("root") as HTMLElement
 );
 root.render(
   <React.StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </React.StrictMode>
 );
